daae1d33cd61a87003887a48e1489247e4545b67
leakp <lea.kampermann@gmail.com>
2016-08-23 18:13:06 +0200

    debugged at thermode, verbose option nicer

--- serialcom.m
+++ serialcom.m
@@ -19,13 +19,17 @@ function [out] = serialcom(s,cmd,varargin)
 % out = serialcom(s,'T',35,'verbose')
 % out = serialcom(s,'HELP',[],'verbose')
 out  = [];
-mspb = 11/s.BaudRate; %muS per byte (comes from baudrate) (1/BaudRate * 11 bits per byte)
+buffer_warn  = 0;
+suppress_out = 0; %suppress output even for DIAG and HELP
+% mspb = 11/s.BaudRate; %muS per byte (comes from baudrate) (1/BaudRate * 11 bits per byte)
 
 
 % clear buffer by reading out potential leftovers
 while s.BytesAvailable ~= 0
     fread(s,s.BytesAvailable);
-    warning('Detected and cleaned buffer leftovers...')
+    if buffer_warn
+        warning('Detected and cleaned buffer leftovers...')
+    end
 end
 
 % differentiate between command types, e.g. 'DIAG' vs 'T;32', what to send
@@ -33,54 +37,33 @@ end
 if any(strcmp(cmd,{'HELP','DIAG','START'}))
     fprintf(s,cmd);
     fprintf('Sending command %s. \n',cmd)
-elseif any(strcmp(cmd,{'T','RoR','SET','MOVE'}))
+elseif any(strcmp(cmd,{'T','ROR','SET','MOVE'}))
     cmdstr = [cmd ';' num2str(varargin{1})];
     fprintf(s,cmdstr);
     fprintf('Sending command %s. \n',cmdstr)
 end
 
-% ensure to wait long enough to get back the Arduino's response (if wanted)
-if length(varargin) ==2
-    switch cmd
-        case 'DIAG'
-            ws = 70*mspb;
-            fprintf('%s, waiting %g seconds to allow Arduino response.\n',cmd,ws)
-            pause(ws)
-        case 'HELP'
-            ws = 326*mspb;
-            fprintf('%s, waiting %g seconds to allow Arduino response.\n',cmd,ws)
-            pause(ws)
-        case 'T'
-            ws = 25*mspb;
-            fprintf('%s, waiting %g seconds to allow Arduino response.\n',cmd,ws)
-            pause(ws)
-        case 'RoR'
-            ws = 18*mspb;
-            fprintf('%s, waiting %g seconds to allow Arduino response.\n',cmd,ws)
-            pause(ws)
-        case 'START'
-            ws = 6*mspb;
-            fprintf('%s, waiting %g seconds to allow Arduino response.\n',cmd,ws)
-            pause(.05)
-        case 'MOVE'
-            ws = 24*mspb;
-            fprintf('%s, waiting %g seconds to allow Arduino response.\n',cmd,ws)
-            pause(ws)
-        case 'SET'
-            ws = 52*mspb;
-            fprintf('%s, waiting %g seconds to allow Arduino response.\n',cmd,ws)
-            pause(ws)
+if any(strcmp(cmd,{'HELP','DIAG',}))
+    fprintf('Asked for %s, waiting %g seconds for Arduino response.\n',cmd,.5)
+    pause(.5)
+    if ~suppress_out
+        fprintf('%s \n',char(fread(s,s.BytesAvailable))')
     end
 end
-%read out Arduino's response from buffer
-
-try
-    out = char(fread(s,s.BytesAvailable))'; %reads as many bytes as stored in buffer
-    if strcmp(varargin{end},'verbose')
-        fprintf('%s \n',out)
+% ensure to wait long enough to get back the Arduino's response (if wanted)
+if length(varargin) == 2
+    ws = .2;
+    fprintf('Verbose wanted, waiting %g seconds for Arduino response.\n',ws)
+    pause(ws)
+    %read out Arduino's response from buffer
+    try
+        out = char(fread(s,s.BytesAvailable))'; %reads as many bytes as stored in buffer
+        if ~suppress_out
+            fprintf('%s \n',out)
+        end
+    catch
+        % e.g. if buffer is empty
+        warning('Problem with s.BytesAvailable, reading buffer was not successful... \n')
     end
-catch
-    % e.g. if buffer is empty
-    fprintf('Problem with s.BytesAvailable... \n')
 end
 end
